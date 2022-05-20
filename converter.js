b683a05efd56ce4dd28ee0810040e766319daa49
Dinuka Malitha <92653525+dinukamalitha@users.noreply.github.com>
2022-05-20 23:55:04 +0530

    Edited the HTML and JS files
    Added decimal to other number systems conversions

--- converter.js
+++ converter.js
@@ -1,10 +1,66 @@
 
-var x = 15;
-var result = ""
+var decimalValue = document.getElementById('decimal');
+var binaryValue = document.getElementById('binary');
+var octalValue = document.getElementById('octal');
+var hexaValue = document.getElementById('hexa');
 
-while (x>0){
-    var y = x%2;
-    result=y+result;
-    x=Math.floor(x/2);
+//==========================Decimal -> Binary ============================
+function decimalToBinary() {
+    var val = decimalValue.value;
+    var result = "";
+    while (val > 0) {
+        var y = val % 2;
+        result = y + result;
+        val = Math.floor(val / 2);
+    }
+    binaryValue.value = result;
+    //console.log(result);
 }
-console.log(result);
+
+
+
+//========================= Decimal -> Octal ==========================
+function decimalToOctal() {
+    var val = decimalValue.value;
+    var result = "";
+    while (val > 0) {
+        var y = val % 8;
+        result = y + result;
+        val = Math.floor(val / 8);
+    }
+    octalValue.value = result;
+    //console.log(result);
+}
+
+//=============================Decimal -> Hexadecimal ==========================
+function decimalToHexa() {
+    var val = decimalValue.value;
+    var result = "";
+    var arr = { 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F' };
+    while (val > 0) {
+        var y = val % 16;
+        if (y > 9) {
+            result = arr[y] + result;
+        }
+        else {
+            result = y + result;
+        }
+        val = Math.floor(val / 16);
+    }
+    hexaValue.value = result;
+}
+
+//===================================
+
+function decimalConvert() {
+    decimalToBinary();
+    decimalToOctal();
+    decimalToHexa();
+};
+
+function Reset() {
+    binaryValue.value = "";
+    octalValue.value = "";
+    decimalValue.value = "";
+    hexaValue.value = "";
+};
