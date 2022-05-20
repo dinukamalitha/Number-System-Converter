687d43c43c548eeaadee01d3b21d50b31d26413c
Dinuka Malitha <92653525+dinukamalitha@users.noreply.github.com>
2022-05-21 00:38:28 +0530

    Added the binary to other number systems conversions

--- converter.js
+++ converter.js
@@ -4,7 +4,8 @@ var binaryValue = document.getElementById('binary');
 var octalValue = document.getElementById('octal');
 var hexaValue = document.getElementById('hexa');
 
-//==========================Decimal -> Binary ============================
+//==========================Decimal -> Binary ===============================
+
 function decimalToBinary() {
     var val = decimalValue.value;
     var result = "";
@@ -14,12 +15,10 @@ function decimalToBinary() {
         val = Math.floor(val / 2);
     }
     binaryValue.value = result;
-    //console.log(result);
 }
 
+//========================= Decimal -> Octal ================================
 
-
-//========================= Decimal -> Octal ==========================
 function decimalToOctal() {
     var val = decimalValue.value;
     var result = "";
@@ -29,10 +28,10 @@ function decimalToOctal() {
         val = Math.floor(val / 8);
     }
     octalValue.value = result;
-    //console.log(result);
 }
 
-//=============================Decimal -> Hexadecimal ==========================
+//=============================Decimal -> Hexadecimal =======================
+
 function decimalToHexa() {
     var val = decimalValue.value;
     var result = "";
@@ -50,13 +49,89 @@ function decimalToHexa() {
     hexaValue.value = result;
 }
 
-//===================================
+//==============================Binary -> Decimal ===========================
+
+function binaryToDecimal() {
+    var val = binaryValue.value;
+    var result = 0;
+    var i = val.length-1;
+    var j = 0;
+    
+    while (i > -1) {
+        var y = (Number(val[i])) * (2 ** [j]);
+        result += y;
+        i--;
+        j++;
+    }
+    decimalValue.value = result;
+}
+
+//===============================Binary -> Octal =============================
+
+function binaryToOctal(){
+    var val = binaryValue.value;
+    var result = 0 , finalResult = "";
+    var i = val.length-1;
+    var j = 0;
+    
+    while (i > -1) {
+        var y = (Number(val[i])) * (2 ** [j]);
+        result += y;
+        i--;
+        j++;
+    }
+
+    while ( result> 0) {
+        var y = result % 8;
+        finalResult = y + finalResult;
+        result = Math.floor(result / 8);
+    }
+    octalValue.value = finalResult;
+}
+
+//=================================Binary -> Hexa ===========================
+
+function binaryToHexa(){
+    var val = binaryValue.value;
+    var result = 0 , finalResult = "";
+    var i = val.length-1;
+    var j = 0;
+    var arr = { 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F' };
+    
+    while (i > -1) {
+        var y = (Number(val[i])) * (2 ** [j]);
+        result += y;
+        i--;
+        j++;
+    }
+
+    while (result > 0) {
+        var y = result % 16;
+        if (y > 9) {
+            finalResult = arr[y] + finalResult;
+        }
+        else {
+            finalResult = y + finalResult;
+        }
+        result = Math.floor(result / 16);
+    }
+
+    hexaValue.value = finalResult;
+}
+
+//=================================Events==============================
 
 function decimalConvert() {
     decimalToBinary();
     decimalToOctal();
     decimalToHexa();
 };
+
+function binaryConvert(){
+    binaryToOctal();
+    binaryToDecimal();
+    binaryToHexa();
+}
 
 function Reset() {
     binaryValue.value = "";
